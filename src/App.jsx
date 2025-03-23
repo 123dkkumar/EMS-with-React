c67843193d0555660dd503b69e4b7aba9eb1adca
123dkkumar <deepakkumar.258105@gmail.com>
2025-03-23 22:00:36 +0530

    work on Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import "./App.css";
 import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <div>
-        <Login />
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/adminlogin" element={<Login />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
